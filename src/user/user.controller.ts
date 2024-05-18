036aa2f5ce1dd0937e218559f70cfdf6b82469bb
Paul-HenryN <ngpaulhenry@gmail.com>
2024-05-18 18:30:15 +0200

    feat: validate email on user creation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { User } from '@prisma/client';
+import { CreateUserDto } from './user.dto';
+import { isValidEmail } from '../utils/validators';
 
 @Controller()
 export class UserController {
     constructor(private userService: UserService) {}
 
     @Post()
-    addUser(@Body() { email }: Pick<User, 'email'>) {
-        this.userService.addUser(email);
+    addUser(@Body() createUserDto: CreateUserDto) {
+        if (!isValidEmail(createUserDto.email)) {
+            throw new BadRequestException('Invalid email');
+        }
+
+        this.userService.addUser(createUserDto.email);
     }
 }
