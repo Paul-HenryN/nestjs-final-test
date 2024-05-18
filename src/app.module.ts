676acb655319616a4cf80c9b8de27055550ab51c
Paul-HenryN <ngpaulhenry@gmail.com>
2024-05-18 16:23:00 +0200

    chore: configure user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { AppRoutingModule } from './app.routing-module';
 import { ConfigurationModule } from './infrastructure/configuration/configuration.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [AppRoutingModule, ConfigurationModule, DatabaseModule],
+    imports: [
+        AppRoutingModule,
+        ConfigurationModule,
+        DatabaseModule,
+        UserModule,
+    ],
 })
 export class AppModule {}
